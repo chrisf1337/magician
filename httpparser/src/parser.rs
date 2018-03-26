ce01d1f77cba7714cbcd4e7cc3b15465cb6b2746
Christopher Fu <chrisf1337@gmail.com>
2018-03-26 01:24:37 -0400

    Implement node parsing for paired tags

--- httpparser/src/parser.rs
+++ httpparser/src/parser.rs
@@ -7,6 +7,38 @@ pub enum Token {
 }
 
 #[derive(Debug, Eq, PartialEq)]
+pub enum NodeType {
+    Html,
+    Text,
+    Head,
+    Body,
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct DomNode {
+    pub node_type: NodeType,
+    pub attrs: Vec<(Token, Token)>,
+    pub pos: Pos,
+    pub children: Vec<DomNode>,
+}
+
+impl DomNode {
+    fn new(
+        node_type: NodeType,
+        attrs: Vec<(Token, Token)>,
+        pos: Pos,
+        children: Vec<DomNode>,
+    ) -> DomNode {
+        DomNode {
+            node_type,
+            attrs,
+            pos,
+            children,
+        }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
 pub enum Error {
     Eof(Pos),
     Unexpected(Pos, String),
@@ -36,7 +68,14 @@ fn consume_whitespace(input: &Vec<char>, (mut index, mut row, mut col): Pos) -> 
 }
 
 fn parse_identifier(input: &Vec<char>, pos: Pos) -> Result<(Token, Pos), Error> {
-    let (mut index, row, mut col) = consume_whitespace(input, pos)?;
+    let pos = consume_whitespace(input, pos)?;
+    parse_identifier_strict(&input, pos)
+}
+
+fn parse_identifier_strict(
+    input: &Vec<char>,
+    (mut index, row, mut col): Pos,
+) -> Result<(Token, Pos), Error> {
     if index >= input.len() {
         return Err(Error::Eof((index, row, col)));
     }
@@ -140,28 +179,155 @@ fn parse_tag_attributes(input: &Vec<char>, pos: Pos) -> Result<(Vec<(Token, Toke
     }
 }
 
-// pub fn parse(input: &String) -> Result<Vec<Token>, (Error, Vec<Token>)> {
-//     let input = input.chars().collect();
-//     let mut pos = (0, 1, 1);
-//     let mut tokens: Vec<Token> = vec![];
-//     loop {
-//         match parse_one_token(&input, pos) {
-//             Err(Error::Eof(_)) => return Ok(tokens),
-//             Err(err) => return Err((err, tokens)),
-//             Ok((Token::Space(_), new_pos)) | Ok((Token::Newline(_), new_pos)) => pos = new_pos,
-//             Ok((Token::LeftAngleBracket(tag_start_pos), tag_name_start_pos)) => {
-//                 match parse_identifier(input, tag_name_start_pos) {
-//                     Ok(Token::Identifier(tag_name_pos, tag_name), tag_name_end_pos) =>
-//                     Ok(_) => unreachable!(),
-//                 }
-//             }
-//             Ok((token, new_pos)) => {
-//                 tokens.push(token);
-//                 pos = new_pos;
-//             }
-//         }
-//     }
-// }
+fn tag_id_str_to_node_type(tag_id_str: &str) -> Option<NodeType> {
+    match tag_id_str.to_ascii_lowercase().as_ref() {
+        "html" => Some(NodeType::Html),
+        "head" => Some(NodeType::Head),
+        "body" => Some(NodeType::Body),
+        _ => None,
+    }
+}
+
+fn parse_opening_tag(input: &Vec<char>, pos: Pos) -> Result<(DomNode, Pos), Error> {
+    let pos = parse_one_char(&input, pos, '<')?;
+    let (index, row, col) = pos;
+    let tag_start_pos = (index - 1, row, col - 1);
+    let (tag_id, pos) = parse_identifier_strict(&input, pos)?;
+    let node_type = match tag_id {
+        Token::Identifier(tag_id_pos, tag_id_str) => match tag_id_str_to_node_type(&tag_id_str) {
+            Some(node_type) => node_type,
+            None => {
+                return Err(Error::Unexpected(
+                    tag_id_pos,
+                    format!("unexpected node type: {}", tag_id_str).to_string(),
+                ))
+            }
+        },
+        _ => unreachable!(),
+    };
+    let (attrs, pos) = parse_tag_attributes(&input, pos)?;
+    let pos = match parse_one_char(&input, pos, '>') {
+        Ok(pos) => pos,
+        Err(Error::Eof(_)) => {
+            return Err(Error::Unexpected(
+                tag_start_pos,
+                format!("unclosed tag: {:?}", node_type),
+            ))
+        }
+        Err(err) => return Err(err),
+    };
+    Ok((DomNode::new(node_type, attrs, tag_start_pos, vec![]), pos))
+}
+
+fn parse_closing_tag(
+    input: &Vec<char>,
+    pos: Pos,
+    opening_tag: DomNode,
+) -> Result<(DomNode, Pos), Error> {
+    let pos = parse_one_char(&input, pos, '<')?;
+    let (index, row, col) = pos;
+    let tag_start_pos = (index - 1, row, col - 1);
+    let pos = parse_one_char(&input, pos, '/')?;
+    let (tag_id, pos) = parse_identifier_strict(&input, pos)?;
+    let node_type = match tag_id {
+        Token::Identifier(tag_id_pos, tag_id_str) => match tag_id_str_to_node_type(&tag_id_str) {
+            Some(node_type) => node_type,
+            None => {
+                return Err(Error::Unexpected(
+                    tag_id_pos,
+                    format!("unexpected node type: {}", tag_id_str).to_string(),
+                ))
+            }
+        },
+        _ => unreachable!(),
+    };
+    if opening_tag.node_type != node_type {
+        return Err(Error::Unexpected(
+            tag_start_pos,
+            format!(
+                "expected closing tag for {:?}, got {:?}",
+                opening_tag.node_type, node_type
+            ).to_string(),
+        ));
+    }
+    let pos = parse_one_char(&input, pos, '>')?;
+    Ok((opening_tag, pos))
+}
+
+fn parse_text_node(
+    input: &Vec<char>,
+    (mut index, mut row, mut col): Pos,
+) -> Result<(DomNode, Pos), Error> {
+    let mut text: Vec<char> = vec![];
+    let start_pos = (index, row, col);
+    while index < input.len() && input[index] != '<' {
+        text.push(input[index]);
+        index += 1;
+        match input[index] {
+            '\n' => {
+                row += 1;
+                col = 1;
+            }
+            _ => col += 1,
+        }
+    }
+    Ok((
+        DomNode::new(NodeType::Text, vec![], start_pos, vec![]),
+        (index, row, col),
+    ))
+}
+
+fn parse_node(input: &Vec<char>, pos: Pos) -> Result<(DomNode, Pos), Error> {
+    let (mut node, mut pos) = parse_opening_tag(&input, pos)?;
+    let node_start_pos = node.pos;
+    loop {
+        pos = match consume_whitespace(&input, pos) {
+            Ok(pos) => pos,
+            Err(Error::Eof(_)) => {
+                return Err(Error::Unexpected(
+                    node_start_pos,
+                    format!("unclosed element: {:?}", node),
+                ))
+            }
+            Err(e) => return Err(e),
+        };
+        let (index, _, _) = pos;
+        if index >= input.len() {
+            return Err(Error::Unexpected(
+                node_start_pos,
+                format!("unclosed element: {:?}", node),
+            ));
+        }
+        match input[index] {
+            '<' => {
+                if index + 1 >= input.len() {
+                    return Err(Error::Unexpected(
+                        node_start_pos,
+                        format!("unclosed element: {:?}", node),
+                    ));
+                }
+                if input[index + 1] == '/' {
+                    return parse_closing_tag(&input, pos, node);
+                }
+                let (child_node, new_pos) = parse_node(&input, pos)?;
+                node.children.push(child_node);
+                pos = new_pos;
+            }
+            _ => {
+                let (text_node, new_pos) = parse_text_node(&input, pos)?;
+                node.children.push(text_node);
+                pos = new_pos;
+            }
+        }
+    }
+}
+
+pub fn parse(input: &String) -> Result<DomNode, Error> {
+    let input = input.chars().collect();
+    let pos = (0, 1, 1);
+    let (node, _) = parse_node(&input, pos)?;
+    Ok(node)
+}
 
 #[cfg(test)]
 mod tests {
@@ -314,7 +480,7 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_tag_attributes_early_termination() {
+    fn test_parse_tag_attributes_early_termination2() {
         let input = "a='a <";
         assert_eq!(
             parse_tag_attributes(&input.chars().collect(), (0, 1, 1)),
@@ -322,74 +488,154 @@ mod tests {
         );
     }
 
-    // #[test]
-    // fn test_parse1() {
-    //     let input = "<html>".to_string();
-    //     assert_eq!(
-    //         parse(&input),
-    //         Ok(vec![
-    //             Token::LeftAngleBracket((0, 1, 1)),
-    //             Token::Identifier((1, 1, 2), "html".to_string()),
-    //             Token::RightAngleBracket((5, 1, 6)),
-    //         ])
-    //     );
-    // }
+    #[test]
+    fn test_parse_opening_tag() {
+        let input = "<html>".to_string();
+        assert_eq!(
+            parse_opening_tag(&input.chars().collect(), (0, 1, 1)),
+            Ok((
+                DomNode::new(NodeType::Html, vec![], (0, 1, 1), vec![]),
+                (6, 1, 7)
+            ))
+        );
+    }
 
-    // #[test]
-    // fn test_parse2() {
-    //     let input = "</html>".to_string();
-    //     assert_eq!(
-    //         parse(&input),
-    //         Ok(vec![
-    //             Token::LeftAngleBracket((0, 1, 1)),
-    //             Token::Slash((1, 1, 2)),
-    //             Token::Identifier((2, 1, 3), "html".to_string()),
-    //             Token::RightAngleBracket((6, 1, 7)),
-    //         ])
-    //     );
-    // }
+    #[test]
+    fn test_parse_opening_tag_strict_whitespace() {
+        let input = "< html>".to_string();
+        assert_eq!(
+            parse_opening_tag(&input.chars().collect(), (0, 1, 1)),
+            Err(Error::Unexpected(
+                (1, 1, 2),
+                "expected identifier".to_string()
+            ))
+        );
+    }
 
-    // #[test]
-    // fn test_parse3() {
-    //     let input = "<div class=\"test\">".to_string();
-    //     //           01   5    1 1      1
-    //     //                     0 1      7
-    //     assert_eq!(
-    //         parse(&input),
-    //         Ok(vec![
-    //             Token::LeftAngleBracket((0, 1, 1)),
-    //             Token::Identifier((1, 1, 2), "div".to_string()),
-    //             Token::Identifier((5, 1, 6), "class".to_string()),
-    //             Token::Equal((10, 1, 11)),
-    //             Token::Str((11, 1, 12), "test".to_string()),
-    //             Token::RightAngleBracket((17, 1, 18)),
-    //         ])
-    //     );
-    // }
+    #[test]
+    fn test_parse1() {
+        let input = "<html></html>".to_string();
+        assert_eq!(
+            parse(&input),
+            Ok(DomNode::new(NodeType::Html, vec![], (0, 1, 1), vec![]))
+        );
+    }
 
-    // #[test]
-    // fn test_parse_ignores_whitespace() {
-    //     let input = "   asdf".to_string();
-    //     assert_eq!(
-    //         parse(&input),
-    //         Ok(vec![Token::Identifier((3, 1, 4), "asdf".to_string())])
-    //     );
-    // }
+    #[test]
+    fn test_parse_text_node1() {
+        let input = "<html>hello</html>".to_string();
+        assert_eq!(
+            parse(&input),
+            Ok(DomNode::new(
+                NodeType::Html,
+                vec![],
+                (0, 1, 1),
+                vec![DomNode::new(NodeType::Text, vec![], (6, 1, 7), vec![])]
+            ))
+        );
+    }
 
-    // #[test]
-    // fn test_parse_increments_row_on_newline() {
-    //     let input = indoc!(
-    //         "
-    //         asdf
-    //         asdf
-    //     "
-    //     ).to_string();
-    //     assert_eq!(
-    //         parse(&input),
-    //         Ok(vec![
-    //             Token::Identifier((0, 1, 1), "asdf".to_string()),
-    //             Token::Identifier((5, 2, 1), "asdf".to_string()),
-    //         ])
-    //     );
-    // }
+    #[test]
+    fn test_parse_text_node2() {
+        let input = "<html> hello    </html>".to_string();
+        assert_eq!(
+            parse(&input),
+            Ok(DomNode::new(
+                NodeType::Html,
+                vec![],
+                (0, 1, 1),
+                vec![DomNode::new(NodeType::Text, vec![], (7, 1, 8), vec![])]
+            ))
+        );
+    }
+
+    #[test]
+    fn test_parse_multiple_nested_nodes() {
+        let input = "<html> hello hello <body></body>  </html>".to_string();
+        assert_eq!(
+            parse(&input),
+            Ok(DomNode::new(
+                NodeType::Html,
+                vec![],
+                (0, 1, 1),
+                vec![
+                    DomNode::new(NodeType::Text, vec![], (7, 1, 8), vec![]),
+                    DomNode::new(NodeType::Body, vec![], (19, 1, 20), vec![]),
+                ]
+            ))
+        );
+    }
+
+    #[test]
+    fn test_parse_multiple_nested_nodes_attrs() {
+        let input = "<html>  <body a='b'  c=\"d\"></body>  </html>".to_string();
+        assert_eq!(
+            parse(&input),
+            Ok(DomNode::new(
+                NodeType::Html,
+                vec![],
+                (0, 1, 1),
+                vec![
+                    DomNode::new(
+                        NodeType::Body,
+                        vec![
+                            (
+                                Token::Identifier((14, 1, 15), "a".to_string()),
+                                Token::Str((16, 1, 17), "b".to_string()),
+                            ),
+                            (
+                                Token::Identifier((21, 1, 22), "c".to_string()),
+                                Token::Str((23, 1, 24), "d".to_string()),
+                            ),
+                        ],
+                        (8, 1, 9),
+                        vec![],
+                    ),
+                ]
+            ))
+        );
+    }
+
+    #[test]
+    fn test_parse_fail_eof1() {
+        let input = "<html".to_string();
+        assert_eq!(
+            parse(&input),
+            Err(Error::Unexpected(
+                (0, 1, 1),
+                format!("unclosed tag: {:?}", NodeType::Html)
+            ))
+        )
+    }
+
+    #[test]
+    fn test_parse_fail_eof2() {
+        let input = "<html>".to_string();
+        assert_eq!(
+            parse(&input),
+            Err(Error::Unexpected(
+                (0, 1, 1),
+                format!(
+                    "unclosed element: {:?}",
+                    DomNode::new(NodeType::Html, vec![], (0, 1, 1), vec![])
+                )
+            ))
+        )
+    }
+
+    #[test]
+    fn test_parse_fail_mismatched_closing_tag() {
+        let input = "<html></body>".to_string();
+        assert_eq!(
+            parse(&input),
+            Err(Error::Unexpected(
+                (6, 1, 7),
+                format!(
+                    "expected closing tag for {:?}, got {:?}",
+                    NodeType::Html,
+                    NodeType::Body
+                )
+            ))
+        )
+    }
 }
